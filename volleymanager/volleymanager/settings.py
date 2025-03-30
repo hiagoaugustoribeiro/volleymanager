2d6c9ecc38f9285c884a624a24a1e0f7d24aee32
hiagoaugustoribeiro <hiagoribeiro_sp@hotmail.com>
2025-03-30 11:48:41 -0300

    update

--- volleymanager/volleymanager/settings.py
+++ volleymanager/volleymanager/settings.py
@@ -26,12 +26,14 @@ SECRET_KEY = 'django-insecure-ejod4ws94_if9^zp2#ty$=b#b2e9113o%wq&mv*t^=n75d^=s-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    ["corsheaders"]
+    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -46,8 +48,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+MIDDLEWARE.insert(1, "whitenoise.middleware.WhiteNoiseMiddleware")
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -55,7 +57,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    
 
 ]
 CORS_ALLOW_ALL_ORIGINS = True
@@ -85,13 +86,11 @@ WSGI_APPLICATION = 'volleymanager.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'default': dj_database_url.config(
-        # Replace this value with your local database's connection string.
-        default='postgresql://postgres:postgres@localhost:5432/mysite',
+    'default': dj_database_url.config(
+        
+        default='postgresql://volleymanager_user:Np9R8qF6LN0xYznqphnGIYPlvAnFLhc1@dpg-cvklab8dl3ps738aa4lg-a.oregon-postgres.render.com/volleymanager',
         conn_max_age=600
-        )
-    }
+    )
 }
 
 
@@ -130,12 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-if not DEBUG:
-    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
-    # and renames the files with unique names for each version to support long-term caching
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
